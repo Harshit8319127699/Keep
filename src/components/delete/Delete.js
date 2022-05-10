488f02f0dda74dc2162d12f7124e3ff96f310f0a
Harshit Vidhyarthi <88877088+Harshit8319127699@users.noreply.github.com>
2022-05-10 22:49:23 +0530

    Update Delete.js

--- src/components/delete/Delete.js
+++ src/components/delete/Delete.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import "../delete/Delete.css"
+function Delete(props) {
+const navigate=useNavigate()
 
-function Delete() {
+  const {  noteslist, setnoteslist, trash, settrash } = { ...props.name }
+  const deletenotes=(e)=>{
+let index=e.target.id
+let arr2=[...noteslist]
+arr2.splice(index,1)
+setnoteslist(arr2)
+navigate("/")
+  }
+  
+  
+  let hold2=noteslist.map((elem,id)=>{
+    return <div className="notes2"  onClick={deletenotes} key={id} id={id}>
+    <h5>Title:{elem.title}</h5>
+    <p>notes :{elem.notes}</p>
+  </div>
+  })
+ 
   return (
-    <div>Delete</div>
+    <>
+    <div className='deletecontainer'>
+      <div className='heading1'>
+        <h1>Delete Your Notes</h1>
+      </div>
+<div className="notesholder2">
+{hold2}
+</div>
+    </div>
+    
+    </>
   )
 }
 
